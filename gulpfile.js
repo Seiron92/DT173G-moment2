c102ad15d99f31f04e04119c838183d8e853853e
Seiron92 <42301363+Seiron92@users.noreply.github.com>
2019-09-12 12:55:24 +0200

    Livereload enabled

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/* SETUP for future reference
+/* SETUP 
 RUN:
 npm init
 npm install -g gulp-cli
@@ -9,69 +9,81 @@ Don't forget to init git, create .gitignore and add node_modules in this file.
 Install all (npm)packages (minify, etc) and declaire them.
 
 execute with: npm run gulp
-
-IF REGULAR GIT PUSH DOESN'T WORK : git push -f origin master
 */
 
-const { src, dest, series, parallel } = require("gulp");
+const {src, dest, series, parallel} = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require('gulp-uglify-es').default;
 const watch = require('gulp-watch');
 const concatCss = require('gulp-concat-css');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
-
+const livereload = require('gulp-livereload');
 // SÖKVÄGAR
 const files = {
     htmlPath: "src/**/*.html",
     jsPath: "src/**/*.js",
     cssPath: "src/**/*.css",
-    imgPath: "src/**/images"
-};
+imgPath :"src/images"
+    };
 
-// TASK: Copy HTML files from src to pub
-function copyHTML() {
+
+    // TASK: Copy HTML files from src to pub
+function copyHTML(){
+    
     return src(files.htmlPath)
-        .pipe(dest("pub")
-        );
-}
+    .pipe(livereload())
+    .pipe(dest("pub")
+   
+    );
+    }
 
-// TASK : Concat and minify js-files and copy to pub
+// TASK : Concat and minify js-files
 
-function jsTask() {
+function jsTask(){
     return src(files.jsPath)
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(dest('pub/js')
-        );
-}
+    .pipe(livereload())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(dest('pub/js')
 
-// TASK : Copy and minify CSS
+    );
+    }
+    
+    // TASK : Move Css to pub, minify files 
 
-function cssTask() {
+function cssTask(){
     return src(files.cssPath)
-        .pipe(concatCss("css/main.css"))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(dest('pub')
-        );
-}
+    .pipe(livereload())
+ .pipe(concatCss("css/main.css"))
+ .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('pub')
+   );
+    }
 // TASK : Minify images and copy from src to pub
 
 function imgsPath() {
     return src(files.imgPath)
+    .pipe(livereload())
         .pipe(imagemin())
         .pipe(dest('pub')
+
         );
 }
 
+
 // TASK : watcher
 function watchTask() {
+    
     watch([files.htmlPath, files.jsPath, files.cssPath, files.imgPath],
-        parallel(copyHTML, jsTask, cssTask, imgsPath)
+        parallel(copyHTML, jsTask, cssTask, imgsPath),
+        livereload.listen()
+        
     );
 }
 
 exports.default = series(
     parallel(copyHTML, jsTask, cssTask, imgsPath),
     watchTask
+
 );
